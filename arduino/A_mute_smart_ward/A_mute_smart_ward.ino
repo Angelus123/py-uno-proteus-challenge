73812abfd2f03f751608a28c511d0d0a0f70590c
emmyn5600 <emmy66418@gmail.com>
2022-01-02 22:29:09 +0200

    Integrate GSM in project

--- arduino/A_mute_smart_ward/A_mute_smart_ward.ino
+++ arduino/A_mute_smart_ward/A_mute_smart_ward.ino
@@ -1,11 +1,19 @@
-const int led = 11;
-const int led1 = 12;
-const int led2 = 13;
+#include <GSM.h>
 
+#define PINNUMBER ""
+const int led = 6;
+const int led1 = 7;
+const int led2 = 5;
+
+
+// initialize the library instance
+GSM gsmAccess;
+GSM_SMS sms;
 int receivedData = 0;
 
 void setup()
 {
+
   pinMode(led, OUTPUT);
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
@@ -13,11 +21,29 @@ void setup()
   digitalWrite (led1, LOW);
   digitalWrite (led2, LOW);
   Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
   Serial.println("Connected Successfully");
+  Serial.println("SMS Messages Sender");
+  boolean notConnected = true;
+
+  // Start GSM shield
+  // If your SIM has PIN, pass it as a parameter of begin() in quotes
+  while (notConnected) {
+    if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
+      notConnected = false;
+    } else {
+      Serial.println("Not connected");
+      delay(1000);
+    }
+  }
+
+  Serial.println("GSM initialized");
 }
 
+
 void loop(){
-  
   while(Serial.available())
   {
     receivedData = Serial.read();
@@ -25,18 +51,28 @@ void loop(){
   
   if (receivedData == 'd')
   {
+    sms.beginSMS("0785182823");
+    sms.print("Hey Izere I am testing!");
+    sms.endSMS();
+    Serial.println("\nWOW!\n");
     digitalWrite (led, HIGH);
     digitalWrite (led1, LOW);
     digitalWrite (led2, LOW);
   }
   else if (receivedData == 'c')
   {
+    sms.beginSMS("0781917267");
+    sms.print("Hey Cyifuzo  I am testing!");
+    sms.endSMS();
     digitalWrite (led, LOW);
     digitalWrite (led1, HIGH);
     digitalWrite (led2, LOW);
   }
   else if (receivedData == 'r')
   {
+     sms.beginSMS("0780837606");
+     sms.print("Hey Odile I am testing!");
+     sms.endSMS();
     digitalWrite (led, LOW);
     digitalWrite (led1, LOW);
     digitalWrite (led2, HIGH);
