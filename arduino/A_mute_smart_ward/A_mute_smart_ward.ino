8d5f2ce8838265dac287576f7ea06917d7b8be20
emmyn5600 <izerefaifelix@gmail.com>
2022-01-02 23:32:14 +0200

    Adding calling functionality

--- arduino/A_mute_smart_ward/A_mute_smart_ward.ino
+++ arduino/A_mute_smart_ward/A_mute_smart_ward.ino
@@ -1,5 +1,6 @@
 #include <GSM.h>
 
+
 #define PINNUMBER ""
 const int led = 6;
 const int led1 = 7;
@@ -9,8 +10,15 @@ const int led2 = 5;
 // initialize the library instance
 GSM gsmAccess;
 GSM_SMS sms;
+GSMVoiceCall vcs;
 int receivedData = 0;
+String remoteNumber = "0785182823";  // the number you will call
+char charbuffer[20];
+String remoteNumbero = "0781917267"; 
+char charbuffero[20];
 
+String remoteNumbert = "0780837606"; 
+char charbuffert[20];
 void setup()
 {
 
@@ -24,8 +32,9 @@ void setup()
    while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  Serial.println("Connected Successfully");
-  Serial.println("SMS Messages Sender");
+  Serial.println("Make Voice Call");
+
+  // connection state
   boolean notConnected = true;
 
   // Start GSM shield
@@ -39,11 +48,13 @@ void setup()
     }
   }
 
-  Serial.println("GSM initialized");
+  Serial.println("GSM initialized.");
+
 }
-
-
 void loop(){
+  Serial.print("Calling to : ");
+        
+      
   while(Serial.available())
   {
     receivedData = Serial.read();
@@ -52,33 +63,70 @@ void loop(){
   if (receivedData == 'd')
   
   {
-    sms.beginSMS("0785182823");
-    sms.print("Hey Izere I am testing!");
-    sms.endSMS();
-    Serial.println("\nWOW!\n");
-    digitalWrite (led, HIGH);
-    digitalWrite (led1, LOW);
-    digitalWrite (led2, LOW);
-    delay(1000)
+    Serial.println(remoteNumber);
+     Serial.println(remoteNumbero);
+      Serial.println(remoteNumbert);
+        Serial.println();
+
+        // Call the remote number
+        remoteNumber.toCharArray(charbuffer, 20);
+        remoteNumbero.toCharArray(charbuffer, 20);
+        remoteNumbert.toCharArray(charbuffer, 20);
+
+
+        // Check if the receiving end has picked up the call
+        if (vcs.voiceCall(charbuffer)) {
+          Serial.println("Call Established. Enter line to end");
+          // Wait for some input from the line
+          while ((vcs.getvoiceCallStatus() == TALKING));
+          // And hang up
+          vcs.hangCall();
+        }
+        Serial.println("Call Finished");
+        remoteNumber = "";
+         
+        Serial.println("Enter phone number to call.");
   }
   else if (receivedData == 'c')
   {
-    sms.beginSMS("0781917267");
-    sms.print("Hey Cyifuzo  I am testing!");
-    sms.endSMS();
-    digitalWrite (led, LOW);
-    digitalWrite (led1, HIGH);
-    digitalWrite (led2, LOW);
-    delay(1000)
+    Serial.println(remoteNumbero);
+        Serial.println();
+
+        // Call the remote number
+        remoteNumbero.toCharArray(charbuffer, 20);
+
+
+        // Check if the receiving end has picked up the call
+        if (vcs.voiceCall(charbuffer)) {
+          Serial.println("Call Established. Enter line to end");
+          // Wait for some input from the line
+          while ((vcs.getvoiceCallStatus() == TALKING));
+          // And hang up
+          vcs.hangCall();
+        }
+        Serial.println("Call Finished");
+        remoteNumber = "";
+        Serial.println("Enter phone number to call.");
   }
   else if (receivedData == 'r')
   {
-     sms.beginSMS("0780837606");
-     sms.print("Hey Odile I am testing!");
-     sms.endSMS();
-    digitalWrite (led, LOW);
-    digitalWrite (led1, LOW);
-    digitalWrite (led2, HIGH);
-    delay(1000)
+     Serial.println(remoteNumbert);
+        Serial.println();
+
+        // Call the remote number
+        remoteNumbert.toCharArray(charbuffer, 20);
+
+
+        // Check if the receiving end has picked up the call
+        if (vcs.voiceCall(charbuffer)) {
+          Serial.println("Call Established. Enter line to end");
+          // Wait for some input from the line
+          while ((vcs.getvoiceCallStatus() == TALKING));
+          // And hang up
+          vcs.hangCall();
+        }
+        Serial.println("Call Finished");
+        remoteNumber = "";
+        Serial.println("Enter phone number to call.");
   }
 }
